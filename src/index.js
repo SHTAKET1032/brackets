072f41fc30b1d3578d76fb6f7a7f05d8e95ff72c
SHTAKET1032 <SHTAKET1032@yandex.ru>
2023-08-18 12:41:06 +0300

    feat: add complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let openBracket = bracketsConfig[j][0];
+            let closeBracket = bracketsConfig[j][1];
+
+            if (char === openBracket) {
+                stack.push(char);
+            } else if (char === closeBracket) {
+                if (stack.length === 0 || stack[stack.length - 1] !== openBracket) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
